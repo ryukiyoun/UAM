71396dcb7e307836c92a526262df4cb48c688d52
ryukiyoun <ryukiyoun@naver.com>
2022-04-14 00:43:41 +0900

    feat : Main Method Commit
    - Main Method Commit

---
+++ src/test/java/universty/assignment/management/ManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package universty.assignment.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
